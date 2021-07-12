c9a648d81c0e9ea5ebc532fcdd256e21d4d82323
Diep Tran <diep.trantuan@gameloft.com>
2021-07-12 10:31:44 +0700

    * adding code for pixel2xl

--- princesstale.py
+++ princesstale.py
@@ -6,6 +6,7 @@ import threading
 from PIL import Image
 import time
 
+is_pixel = True
 
 def connect_device():
     print("start to connect to device")
@@ -22,10 +23,14 @@ def connect_device():
     return devices[0]
 
 def retry_find(d, delay):
-    received_but = (600, 1082)
-    tap_anywhere = (600, 1000)
-    tap_to_see = (460, 1240)
-
+    if is_pixel:
+        received_but = (1000, 1900)
+        tap_anywhere = (1000, 1900)
+        tap_to_see = (720, 2360)
+    else:
+        received_but = (600, 1082)
+        tap_anywhere = (600, 1000)
+        tap_to_see = (460, 1240)
     click(d, received_but[0], received_but[1], delay)
     click(d, tap_anywhere[0], tap_anywhere[1], delay)
     click(d, tap_to_see[0], tap_to_see[1], delay)
@@ -36,15 +41,6 @@ def click(d, x, y, delay):
     d.shell(f'input tap {x} {y}')
 
 device = connect_device()
-# ss = device.screencap()
-# with open("screen.png", "wb") as fp:
-#     fp.write(ss)
-
-
-# sct = mss.mss()
-a = 10
-b = 10
-c = 100
 
 red_threshold = 2239
 
@@ -52,40 +48,38 @@ while True:
     time.sleep(3)
     image = device.screencap()
     img = cv2.imdecode(np.frombuffer(image, np.uint8), cv2.IMREAD_COLOR)
-    image = cv2.resize(img, (450, 800))
-    # image_check = cv2.rectangle(image, (330, 360), (380, 410), (255, 0, 0), 2)
-    image = image[350:420, 320:390]
-
-    # hsvFrame = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
-
-    # img  = np.array(scr)
+    # pixel 2 xl = 2880 x 1440
+    if is_pixel:
+        image = img[1300:1490, 1040:1230]
+        hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+        red_lower = np.array([169, 100, 100])
+        red_upper = np.array([189, 255, 255])
+        red_mask = cv2.inRange(hsv, red_lower, red_upper)
+    else:
+        image = cv2.resize(img, (450, 800))
+        image = image[350:420, 320:390]
+        red_lower = np.array([10, 10, 100], np.uint8)
+        red_upper = np.array([200, 200, 255], np.uint8)
+        red_mask = cv2.inRange(image, red_lower, red_upper)
+        output = cv2.bitwise_and(image, image, mask=red_mask)
     
-    # if cv2.waitKey(25) == ord('a'):
-    #     a = a+10
-    # if cv2.waitKey(25) == ord('b'):
-    #     b = b+10
-    # if cv2.waitKey(25) == ord('c'):
-    #     c = c+10
+    # if not is_pixel:
+        # cv2.imshow('output', np.hstack([image, output]))
+    # else:
+        # cv2.imshow('mask', red_mask)
 
-    red_lower = np.array([a, b, c], np.uint8)
-    
-    red_upper = np.array([200, 200, 255], np.uint8)
-    red_mask = cv2.inRange(image, red_lower, red_upper)
-    output = cv2.bitwise_and(image, image, mask=red_mask)
-
-
-    
-
-    cv2.imshow('output', np.hstack([image, output]))
-
-    if cv2.waitKey(25) == ord('q'):
-        cv2.destroyAllWindows()
-        break
+    # if cv2.waitKey(25) == ord('q'):
+        # cv2.destroyAllWindows()
+        # break
     
     count_red = cv2.countNonZero(red_mask)
     print(count_red)
-    if count_red >= red_threshold and count_red != 2377 and count_red != 2365 and count_red != 2317:
+    if count_red >= red_threshold and count_red != 2377 and count_red != 2365 and count_red != 2317 and is_pixel == False:
         print("FOUND")
+        found = True
+        break
+    elif is_pixel and count_red >= 2100:
+        print("Found in pixel2")
         found = True
         break
     else:
